1886fceee88e18600fe7c0617a950f70c68e6cc1
sweeppy <104524272+sweeppy@users.noreply.github.com>
2025-11-04 19:17:35 +0300

    Initial

---
+++ build/CMakeFiles/Lab.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/app.cpp" "CMakeFiles/Lab.dir/src/app.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/app.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/graycode.cpp" "CMakeFiles/Lab.dir/src/graycode.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/graycode.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/helpers.cpp" "CMakeFiles/Lab.dir/src/helpers.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/helpers.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/info.cpp" "CMakeFiles/Lab.dir/src/info.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/info.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/io.cpp" "CMakeFiles/Lab.dir/src/io.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/io.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/main.cpp" "CMakeFiles/Lab.dir/src/main.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/main.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/menu.cpp" "CMakeFiles/Lab.dir/src/menu.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/menu.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/multiset.cpp" "CMakeFiles/Lab.dir/src/multiset.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/multiset.cpp.o.d"
+  "/Users/vladimirkonotopov/Desktop/GitHubCloneRepositories/Gray_code/src/random.cpp" "CMakeFiles/Lab.dir/src/random.cpp.o" "gcc" "CMakeFiles/Lab.dir/src/random.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Lab.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab.dir/src/app.cpp.o"
+  "CMakeFiles/Lab.dir/src/app.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/graycode.cpp.o"
+  "CMakeFiles/Lab.dir/src/graycode.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/helpers.cpp.o"
+  "CMakeFiles/Lab.dir/src/helpers.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/info.cpp.o"
+  "CMakeFiles/Lab.dir/src/info.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/io.cpp.o"
+  "CMakeFiles/Lab.dir/src/io.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/main.cpp.o"
+  "CMakeFiles/Lab.dir/src/main.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/menu.cpp.o"
+  "CMakeFiles/Lab.dir/src/menu.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/multiset.cpp.o"
+  "CMakeFiles/Lab.dir/src/multiset.cpp.o.d"
+  "CMakeFiles/Lab.dir/src/random.cpp.o"
+  "CMakeFiles/Lab.dir/src/random.cpp.o.d"
+  "Lab"
+  "Lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
